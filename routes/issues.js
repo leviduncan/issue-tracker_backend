cd3f415346397175bc1f764bf422e52ef765fd2a
darrin@darrinduncan.com <darrin@darrinduncan.com>
2022-08-13 00:54:28 -0400

    Add update route using put

--- routes/issues.js
+++ routes/issues.js
@@ -59,6 +59,15 @@ router.patch("/:id", async (req, res) => {
     }
 })
 
+router.put("/:id", async (req, res) => {
+    try{
+        const updateItem = await Issue.findByIdAndUpdate(req.params.id, {$set: req.body})
+        res.status(200).json('Item Updated')
+    }catch(err){
+        console.log("Error: " + err)
+    }
+})
+
 router.delete('/:id', async(req, res) => {
     try{
         const issues = await Issue.findById(req.params.id)
